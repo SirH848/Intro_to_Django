29027a836767259a41783e7d16b40eddcaf64453
SirH848/Henry <henriventures@gmail.com>
2024-09-19 22:05:23 +0100

    Created simple APIs

--- books/tests.py
+++ books/tests.py
@@ -8,20 +8,19 @@ class BookTests(TestCase):
     def setUpTestData(cls):
         cls.book = Book.objects.create(
             title='My title',
-            subtitle='a subtitle',
+            subtitle='My subtitle',
             author='Tom and Jerry',
             isbn='123456789123'
         )
 
     def test_book_content(self):
         self.assertEqual(self.book.title, 'My title')
-        self.assertEqual(self.book.subtitle, 'a subtitle')
+        self.assertEqual(self.book.subtitle, 'My subtitle')
         self.assertEqual(self.book.author, 'Tom and Jerry')
         self.assertEqual(self.book.isbn, '123456789123')
     
     def test_book_listview(self):
         response = self.client.get(reverse('home'))
-
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'a subtitle')
+        self.assertContains(response, 'My subtitle')
         self.assertTemplateUsed(response, 'books/book_list.html')
--- books/views.py
+++ books/views.py
@@ -6,3 +6,4 @@ from .models import Book
 class BookListView(ListView):
     model = Book
     template_name = 'book_list.html'
+    content_object_name = 'my_list_of_books'
